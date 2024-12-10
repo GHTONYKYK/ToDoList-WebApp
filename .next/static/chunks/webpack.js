f8caac50835612e0391a9ead66a2b293e91e1428
Grace_Chae <eunhye.chae@edu.sait.ca>
2024-12-09 21:05:11 -0700

    updated pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("5bb99fe94a0aa858")
+/******/ 		__webpack_require__.h = () => ("235213b51d20c5ca")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
